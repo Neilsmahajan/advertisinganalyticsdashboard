6dd3bffc6643d0344dfa32e521bbaaed043dacad
Neil Mahajan <neilsmahajan@gmail.com>
2025-03-22 13:39:34 -0700

    Simplify Loading component by removing inline styles and retaining basic loading message

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,13 +1,6 @@
 export default function Loading() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-      }}
-    >
+    <div>
       <p>Loading...</p>
     </div>
   );
