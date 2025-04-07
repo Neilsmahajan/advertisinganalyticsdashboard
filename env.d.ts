7f30711b8ec018ddc49a696a053f27764d2c0b90
Neil Mahajan <neilsmahajan@gmail.com>
2025-04-07 07:56:26 -0700

    Remove env.d.ts from version control and update .gitignore to reflect changes

--- env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: "development" | "production";
-    BROWSERLESS_API_KEY?: string;
-    BROWSERLESS_API_URL?: string;
-    // other environment variables
-  }
-}
